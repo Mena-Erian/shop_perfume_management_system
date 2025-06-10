d0ee22d25216e0d50d1d8769c23f394d4a1752e2
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-06-10 15:02:48 +0300

    add cursor in home

--- src/app/pos/home/home.component.ts
+++ src/app/pos/home/home.component.ts
@@ -1,10 +1,106 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Carousel } from 'primeng/carousel';
+import { Tag } from 'primeng/tag';
+
+interface Product {
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  category: string;
+  quantity: number;
+  inventoryStatus: string;
+  rating: number;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule],
+  imports: [ButtonModule, Carousel, Tag, NgStyle],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  products: Product[] = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'perfume1.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1001',
+      code: 'nvklal433',
+      name: 'Classic Perfume',
+      description: 'Elegant and timeless fragrance.',
+      image: 'perfume2.jpg',
+      price: 120,
+      category: 'Fragrance',
+      quantity: 10,
+      inventoryStatus: 'LOWSTOCK',
+      rating: 4,
+    },
+    {
+      id: '1002',
+      code: 'zz21cz3c1',
+      name: 'Modern Scent',
+      description: 'Fresh and modern aroma for daily use.',
+      image: 'perfume3.jpg',
+      price: 90,
+      category: 'Fragrance',
+      quantity: 0,
+      inventoryStatus: 'OUTOFSTOCK',
+      rating: 3,
+    },
+  ];
+  responsiveOptions: any[] | undefined;
+
+  // constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warn';
+      case 'OUTOFSTOCK':
+        return 'danger';
+      default:
+        return 'info'; // default value for unknown status
+    }
+  }
+}
